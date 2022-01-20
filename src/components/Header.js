5bfab717ce9578b2b5013aedae241491d27735b8
Xtabel <xtabelakpo@gmail.com>
2022-01-20 15:21:29 +0100

    Created the home page without the social media links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
+import Grid from '@mui/material/Grid';
+import image from '../assets/image.gif';
+
 import './some.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,14 +20,34 @@ const useStyles = makeStyles((theme) => ({
     justifyContent:'space-evenly',
     // backgroundColor:'blue',
     width:'60%',
-
+  },
+  content:{
+    padding:'50px 100px',
+  },
+  textintro:{
+    fontFamily: 'Charmonman, cursive',
+    fontWeight:'light',
+  },
+  myname:{
+    fontSize:'60px',
+    color:'#263238',
+    marginBottom:'20px',
+  },
+  jobtype:{
+    color:'#263238',
+    lineHeight:"50px"
+  },
+  jobtools:{
+    color:'#525252',
+    width:'65%'
   },
 }));
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <div className={classes.header}>
+   <div>
+      <div className={classes.header}>
       <div className={classes.navlink}>
         <div>
         <div className="thread">
@@ -43,6 +66,23 @@ const Header = () => {
         </div></div>
       </div>
       </div>
+      <div className={classes.content}>
+        <Grid container spacing={3}>
+          <Grid item xs={7} sm={7}>
+             <h3 className={classes.textintro} >Hello, my name is -</h3>
+             <p className={classes.myname}>Christabel Akpoguma</p>
+             <span className={classes.otherIntroDetails}>
+               <h4 className={classes.jobtype}>Product Designer and Creative Front-end Developer</h4>
+               <p className={classes.jobtools}>I work with HTML, CSS , React JS, Adobe Photoshop,  Adobe Illustrator,
+ Adobe Indesign and Figma.</p>
+             </span>
+          </Grid>
+          <Grid item xs={3} sm={3}>
+              <img src={image} alt="Creative"/>
+          </Grid>
+        </Grid>
+      </div>
+   </div>
    
   );
 };
